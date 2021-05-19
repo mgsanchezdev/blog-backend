5631e517ba89e0b4d583e55e0e474a466e9c3190
Mario Gustavo Sanchez <mgsanchezdev@gmail.com>
2021-05-18 22:30:09 -0300

    Configuracion de la arquitectura inicial del proyecto, instalacion de la Express, Eslint, Prittier y generar la primera ruta para probar que esta todo funcionando, probar que el servidor esta arrancando

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Aplicacion funcionando');
+});
+
+app.listen(3000, () => {
+  console.log('Aranco el servidor');
+});

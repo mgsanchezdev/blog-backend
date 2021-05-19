6bf1b92fde082d5a87563844fadb7122bd9dc86f
Mario Gustavo Sanchez <mgsanchezdev@gmail.com>
2021-05-19 09:27:04 -0300

    Logica del endpoind get/posts/:id, busca un post por id que se especifica por parametro. Si existe devuelve todo el detalle del post, sino existe devuelve un error

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -14,4 +14,22 @@ router.get('/', async (req, res) => {
   });
 });
 
+router.get('/:postId', async (req, res) => {
+    const posts = await Post.findOne({
+      where: { id: req.params.postId },
+    });
+
+    if(posts){
+        res.status(200).json({
+            data: posts,
+            message: 'Post listed',
+          });
+    }else{
+        res.status(404).json({
+            message: 'the post does not exist',
+          });
+    }
+    
+});
+
 module.exports = router;

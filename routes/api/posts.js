77b2561a8501260cef169426bd0f832d1fdd938a
Mario Gustavo Sanchez <mgsanchezdev@gmail.com>
2021-05-20 02:55:00 -0300

    POST /posts, guardar un nuevo post según los datos recibidos en la petición, pido que todo los campos son requeridos

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { check, validationResult } = require('express-validator');
 
 const { Post } = require('../../db');
 
@@ -15,21 +16,42 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:postId', async (req, res) => {
-    const posts = await Post.findOne({
-      where: { id: req.params.postId },
-    });
+  const post = await Post.findOne({
+    where: { id: req.params.postId },
+  });
 
-    if(posts){
-        res.status(200).json({
-            data: posts,
-            message: 'Post listed',
-          });
-    }else{
-        res.status(404).json({
-            message: 'the post does not exist',
-          });
-    }
-    
+  if (post) {
+    res.status(200).json({
+      data: post,
+      message: 'Post listed',
+    });
+  } else {
+    res.status(404).json({
+      message: 'the post does not exist',
+    });
+  }
 });
+
+router.post(
+  '/',
+  [
+    check('titulo', 'The title is required').not().isEmpty(),
+    check('contenido', 'The contents is required').not().isEmpty(),
+    check('imagen', 'The image is required').not().isEmpty(),
+    check('categoria', 'The category is required').not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errores: errors.array() });
+    }
+    const post = await Post.create(req.body);
+    res.status(200).json({
+      data: post,
+      message: 'post created',
+    });
+  }
+);
 
 module.exports = router;
